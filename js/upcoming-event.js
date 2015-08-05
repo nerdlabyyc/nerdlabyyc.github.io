7da2ca5aa647dc70034087edaa62a7b892a9dc4e
Heather Tovey <heather@htovey.com>
2015-08-05 13:46:45 -0600

    Update code to include date formatting.

--- js/upcoming-event.js
+++ js/upcoming-event.js
@@ -10,12 +10,12 @@ var getMeetupData = function() {
     $.getJSON(meetupURL, function(data) {
         if (!$.isEmptyObject(data.results)) {
 
-            var d = new Date(data.results[0].time);
-            console.log(d);
+            var d = new Date(data.results[0].time + data.results[0].utc_offset);
+            console.log(data.results[0].utc_offset);
             var eventDescriptionFull = data.results[0].description;
             var eventDescriptionSnippet = eventDescriptionFull.split('</p>');
 
-            eventDate.text(data.results[0].utc_offset);
+            eventDate.text(getDateFormat(d));
             eventLocation.text(data.results[0].venue.name);
             eventTitle.text(data.results[0].name);
             eventDescription.html(eventDescriptionSnippet[0]);
@@ -27,7 +27,68 @@ var getMeetupData = function() {
 };
 
 
+var getDateFormat = function(time) {
 
+    var newTime = '';
+    var month = time.getUTCMonth();
+    var day = time.getUTCDay();
+    var date = time.getUTCDate();
+    var hour = time.getUTCHours();
+    var minute = time.getUTCMinutes();
+    var periodOfDay;
+
+
+
+
+    newTime = getMonthFormat(month) + ' ' + date + ', ' + getHourFormat(hour) + ':' + getMinuteFormat(minute) + getPeriodOfDay(hour);
+    time.getMonth();
+    return newTime;
+};
+
+var getMonthFormat = function(month) {
+    switch (month) {
+        case 0: return 'January';
+        case 1: return 'February';
+        case 2: return 'March';
+        case 3: return 'April';
+        case 4: return 'May';
+        case 5: return 'June';
+        case 6: return 'July';
+        case 7: return 'August';
+        case 8: return 'September';
+        case 9: return 'October';
+        case 10: return 'November';
+        case 11: return 'December';
+    }
+};
+
+var getMinuteFormat = function(minute) {
+    if (minute < 10) {
+        return '0' + minute;
+    } else {
+        return minute;
+    }
+};
+
+var getHourFormat = function(hour) {
+    if (hour > 12) {
+        return hour - 12;
+    } else {
+        return hour;
+    }
+};
+
+var getPeriodOfDay = function(hour) {
+    if (hour > 12) {
+        return 'pm';
+    } else if (hour < 12) {
+        return 'am';
+    } else if (hour === 12) {
+        return 'noon';
+    } else if (hour === 0) {
+        return 'midnight';
+    }
+};
 
 $( document ).ready(function() {
     getMeetupData();
