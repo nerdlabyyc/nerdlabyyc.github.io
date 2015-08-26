095777790e25901bb4bf0bac375fd6b213ddd1a1
Heather Tovey <heather@htovey.com>
2015-08-26 16:04:33 -0600

    Create mobile menu.

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+var manageMenu = function() {
+	$('#menu-button').click(function() {
+		$('#trigger').toggleClass('open');
+	})
+}
+
+
+
+
+
+$( document ).ready(function() {
+    manageMenu();
+});
